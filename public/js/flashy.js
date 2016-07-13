d8d4ac3934f8198b4c6abbf6f86e61949e4b22cd
Akeem McNaught <Akeem McNaught>
2016-07-13 11:37:02 -0400

    commit for mooney

--- public/js/flashy.js
+++ public/js/flashy.js
@@ -1,8 +1,11 @@
 $(document).ready(function(){
-	$("#akeem").mouseenter(function(){
+
+	$("#akeem").fadeTo('slow',.25);
+	$("#akeem").mouseleave(function(){
 		$("#akeem").fadeTo('slow',.25);
 		});
-	$("#akeem").mouseleave(function(){
+	$("#akeem").mouseenter(function(){
 		$("#akeem").fadeTo('fast',1.00);
 	});
 });
+
